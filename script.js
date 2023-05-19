4603c5e5cb03c27e27c745e6fa61a49d6bcd5d22
VDentesano <dentesanovalentino@gmail.com>
2023-05-19 12:54:13 -0300

    Con formulario modal no terminado

--- script.js
+++ script.js
@@ -1,3 +1,154 @@
+$(document).ready(function () {
+
+    let edit = false;
+    fetchTasks ();
+
+    /*$('#search').keyup(function (e) {
+        //Busco el input que tiene como id 'search' y
+        //obtengo su contenido.
+        
+        let search = $('#search').val();
+
+        if (search) {
+            console.log("hl");
+            //Vamos a utilizar un mÃ©todo JQuery llamado ajax.
+            //Dicho mÃ©todo nos permite hacer una peticiÃ³n a un servidor.   
+            //Toma un objeto como parÃ¡metro.
+            $.ajax({
+                url: 'task-search.php', //Lugar donde hacer la peticiÃ³n.
+                type: 'POST', //Tipo de peticiÃ³n.
+                data: {search: search}, //La informaciÃ³n que le envio al servidor.
+                success: function(response) { 
+                    let tasks = JSON.parse(response);   //Si el servidor respondiÃ³ correctamente
+                    //console.log(tasks);               //tengo la informaciÃ³n que me devolviÃ³ el mismo.
+                                                        //Ver que ya no tengo un string, sino que tengo un objeto JSON!
+                    let template = '';
+                    
+                    tasks.forEach(task => {
+                        template += `<li class="list-group-item">${task.name}</li>`;
+                    });
+            
+                    $('#task-result ul').html(template);
+                },
+                error: function (jqXHR, exception) {
+                    console.log(jqXHR);
+                }                                     
+            });
+        }
+        else {
+            $('#task-result ul').html('');
+        }
+    });*/
+
+    //Por defecto, un formulario hace que la pÃ¡gina se refresque ya que
+    //espera, por parte del servidor, que le llegue una pÃ¡gina nueva.
+    //Para corroborar esto escribir: $('#task-form').submit(function (e) {
+    //}); e ir a la pÃ¡gina y hacer click en el botÃ³n "Save Task".
+    $('#task-form').submit(function (e) {
+        e.preventDefault(); //Hacemos que no se refresque la pÃ¡gina por defecto.
+
+        let postData = { //Lo que le enviaremos al servidor.
+            id: $('#taskId').val(),
+            name: $('#name').val(),
+            description: $('#description').val()
+        };
+        //console.log(postData);
+
+        let url = edit === false ? 'task-add.php' : 'task-update.php';
+
+        $.ajax({
+            url: url, 
+            type: 'POST', 
+            data: postData, 
+            success: function(response) { 
+                edit = false; 
+                fetchTasks ();
+                //Al agregar una nueva task y tocar el botÃ³n "Save Task",
+                //reseteo el formulario.
+                $('#task-form').trigger('reset'); 
+            },
+            error: function (jqXHR, exception) {
+                console.log(jqXHR);
+            }                   
+        });
+
+    });
+
+    function fetchTasks () {
+        $.ajax({
+            url: 'task-list.php',
+            type: 'GET',
+            success: function (response) {
+                let tasks = JSON.parse(response);
+                let template = '';
+                tasks.forEach(task => {
+                    template += `
+                        <tr taskId="${task.id}">
+                            <td>${task.id}</td>
+                            <td>
+                                <a href="#" class="task-item">${task.name}</a>
+                            </td>
+                            <td>${task.description}</td>
+                            <td class="align-middle">
+                                <button class="task-delete btn btn-danger">
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>`;
+                });
+                
+                $('#all-tasks').html(template);
+            },
+            error: function (jqXHR, exception) {
+                console.log(jqXHR);
+            }
+        })    
+    }
+
+    $(document).on('click', '.task-delete', function (e) {
+        if (confirm('Are you sure you want to delete it?')) {
+            let element = $(this)[0].parentElement.parentElement;
+            let id = $(element).attr('taskId');
+
+            $.ajax({
+                url: 'task-delete.php',
+                type: 'POST',
+                data: {id: id},
+                success: function (response) {
+                    fetchTasks();     
+                    console.log(response);   
+                },
+                error: function (jqXHR, exception) {
+                    console.log(jqXHR);
+                }
+            });      
+        }
+    });
+
+    $(document).on('click', '.task-item', function () {
+        let element = $(this)[0].parentElement.parentElement;
+        let id = $(element).attr('taskId');
+
+        $.ajax({
+            url: 'task-data.php',
+            type: 'POST',
+            data: {id: id},
+            success: function (response) {
+                let task = JSON.parse(response);
+                $('#taskId').val(task.id); 
+                $('#name').val(task.name);
+                $('#description').val(task.description);  
+                edit = true;    
+            },
+            error: function (jqXHR, exception) {
+                console.log(jqXHR);
+            }
+        });      
+    });
+});
+
+
+
 const search = document.querySelector('.input-group input'),
     table_rows = document.querySelectorAll('tbody tr'),
     table_headings = document.querySelectorAll('thead th');
